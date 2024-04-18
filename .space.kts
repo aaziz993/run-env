bf78fde5bc657547c7273f929a6dad1cd7ce34f9
Atoev Aziz <a.atoev93@gmail.com>
2024-04-19 00:53:25 +0500

    Initial Commit

--- .space.kts
+++ .space.kts
@@ -19,26 +19,28 @@ job("Code analysis, test, build and publish") {
                 tags {
                     // use current job run number as a tag - '0.0.run_number'
                     +"aaziz93.registry.jetbrains.space/p/aaziz-93/containers/cicd-os:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
+                    +"aaziz93.registry.jetbrains.space/p/aaziz-93/containers/cicd-os:latest"
                 }
             }
         }
 
-//        host("Build and push a Docker image to DockerHub") {
-//            // Before running the scripts, the host machine will log in to
-//            // the registries specified in connections.
-//            dockerRegistryConnections {
-//                // specify connection key
-//                +"docker_hub"
-//                // multiple connections are supported
-//                // +"one_more_connection"
-//            }
-//
-//            dockerBuildPush {
-//                labels["vendor"] = "aaziz993"
-//                tags {
-//                    +"aaziz93/cicd-os:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
-//                }
-//            }
-//        }
+        host("Build and push a Docker image to DockerHub") {
+            // Before running the scripts, the host machine will log in to
+            // the registries specified in connections.
+            dockerRegistryConnections {
+                // specify connection key
+                +"docker_hub"
+                // multiple connections are supported
+                // +"one_more_connection"
+            }
+
+            dockerBuildPush {
+                labels["vendor"] = "aaziz993"
+                tags {
+                    +"aaziz93/cicd-os:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
+                    +"aaziz93/cicd-os:latest"
+                }
+            }
+        }
     }
 }
