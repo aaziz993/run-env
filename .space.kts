5a9e2d16217c0d9b7f8a1d61664822735ef24b71
Atoev Aziz <a.atoev93@gmail.com>
2024-04-18 23:03:45 +0500

    Initial Commit

--- .space.kts
+++ .space.kts
@@ -9,36 +9,39 @@ job("Code analysis, test, build and publish") {
         gitPush { enabled = true }
     }
 
-    host("Build and push a Docker image to Space Packages") {
-        dockerBuildPush {
-            // image labels
-            labels["vendor"] = "aaziz993.github.io"
-            // to add a raw list of additional build arguments, use
-            // extraArgsForBuildCommand = listOf("...")
-            // to add a raw list of additional push arguments, use
-            // extraArgsForPushCommand = listOf("...")
-            // image tags
-            tags {
-                // use current job run number as a tag - '0.0.run_number'
-                +"aaziz993.registry.jetbrains.space/p/aaziz-93/containers/cicd-os:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
+    parallel {
+
+        host("Build and push a Docker image to Space Packages") {
+            dockerBuildPush {
+                // image labels
+                labels["vendor"] = "aaziz993.github.io"
+                // to add a raw list of additional build arguments, use
+                // extraArgsForBuildCommand = listOf("...")
+                // to add a raw list of additional push arguments, use
+                // extraArgsForPushCommand = listOf("...")
+                // image tags
+                tags {
+                    // use current job run number as a tag - '0.0.run_number'
+                    +"aaziz993.registry.jetbrains.space/p/aaziz-93/containers/cicd-os:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
+                }
             }
         }
-    }
 
-    host("Build and push a Docker image to DockerHub") {
-        // Before running the scripts, the host machine will log in to
-        // the registries specified in connections.
-        dockerRegistryConnections {
-            // specify connection key
-            +"docker_hub"
-            // multiple connections are supported
-            // +"one_more_connection"
-        }
+        host("Build and push a Docker image to DockerHub") {
+            // Before running the scripts, the host machine will log in to
+            // the registries specified in connections.
+            dockerRegistryConnections {
+                // specify connection key
+                +"docker_hub"
+                // multiple connections are supported
+                // +"one_more_connection"
+            }
 
-        dockerBuildPush {
-            labels["vendor"] = "aaziz993.github.io"
-            tags {
-                +"aaziz993.github.io/cicd-os:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
+            dockerBuildPush {
+                labels["vendor"] = "aaziz993.github.io"
+                tags {
+                    +"aaziz993.github.io/cicd-os:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
+                }
             }
         }
     }
